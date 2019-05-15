f8ce076fa25afe5a96d904b77c21209e581c35b9
Michal Macejko <michal.macejko@3scale.net>
2019-05-15 07:16:39 +0200

    temp

--- app/config/index.ts
+++ app/config/index.ts
@@ -1,9 +1,10 @@
 console.log('process.env', process.env);
 console.log('process.env.API_URL', process.env.API_URL);
+console.log('process.env.CUSTOM_ENV', process.env.CUSTOM_ENV);
 
 // TODO, fixme
 // const apiUrl = process.env.API_URL ? process.env.CUSTOM_ENV : 'http://localhost:3001';
-const apiUrl = 'https://storyvine-api-staging.herokuapp.com';
+const apiUrl = 'https://storyvine-staging.herokuapp.com';
 
 export default {
   bffUri: apiUrl,
