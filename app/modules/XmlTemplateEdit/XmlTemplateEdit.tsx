863118e6aa0be30d80206e1519074f05757f0bbd
Michal Macejko <michal.macejko@3scale.net>
2019-05-15 16:57:38 +0200

    [xml edit] edit page with separated form

--- app/modules/XmlTemplateEdit/XmlTemplateEdit.tsx
+++ app/modules/XmlTemplateEdit/XmlTemplateEdit.tsx
@@ -1,5 +1,80 @@
 import * as React from 'react';
+import { Form, Input, Button } from 'antd';
 
-const XmlTemplateEdit = () => <div />;
+const FormItem = Form.Item;
+const TextArea = Input.TextArea;
+
+import { FormComponentProps } from 'antd/lib/form/Form';
+interface InnerProps { fields:any, onValidSubmit: Function }
+type Props = InnerProps & FormComponentProps;
+
+class XmlTemplateForm extends React.Component<Props> {
+  handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+
+    this.props.form.validateFields((errors, updatedFields) => {
+      if (errors) return;
+
+      this.props.onValidSubmit(updatedFields);
+    });
+  };
+  render() {
+    const {
+      form: { getFieldDecorator },
+      fields: { name, xml }
+    } = this.props;
+
+    return(
+      <div>
+        <Form layout={'vertical'} onSubmit={this.handleSubmit}>
+          <FormItem label='Name'>
+            { getFieldDecorator('name',
+              { rules: [{ required: true }], initialValue: name,
+            })(
+              <Input
+                size='large'
+                placeholder='My super duper XML'
+              />
+            )}
+          </FormItem>
+          <FormItem label='XML'>
+            { getFieldDecorator('xml',
+              { rules: [{ required: true }], initialValue: xml,
+            })(
+              <TextArea
+                rows={20}
+                placeholder='<xml></xml>'
+              />
+            )}
+          </FormItem>
+          <FormItem>
+            <Button
+              type='primary'
+              onClick={this.handleSubmit}>
+              Save
+            </Button>
+          </FormItem>
+        </Form>
+      </div>
+    );
+  };
+};
+
+const XmlTemplateForm1 = Form.create<Props>()(XmlTemplateForm);
+
+class XmlTemplateEdit extends React.Component {
+  onValidSubmit = (updatedFields:any) => {
+    console.log('handleFormChange', updatedFields);
+  };
+  render() {
+    const fields = { name: '', xml: '' };
+
+    return(
+      <div>
+        <XmlTemplateForm1 fields={fields} onValidSubmit={this.onValidSubmit} />
+      </div>
+    );
+  };
+};
 
 export default XmlTemplateEdit;
