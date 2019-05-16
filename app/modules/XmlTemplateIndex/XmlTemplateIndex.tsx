e319f03296632c3dd36fde02be03154a4f54d8e2
Michal Macejko <michal.macejko@3scale.net>
2019-05-16 16:22:28 +0200

    antd table for xml templates index

--- app/modules/XmlTemplateIndex/XmlTemplateIndex.tsx
+++ app/modules/XmlTemplateIndex/XmlTemplateIndex.tsx
@@ -2,27 +2,58 @@ import * as React from 'react';
 import { QUERY_XML_TEMPLATES } from 'store/app';
 import { Link } from 'react-router-dom';
 import { Query } from 'react-apollo';
+import { Table, Pagination } from 'antd';
 
 const XmlTemplateIndex = () => {
 
-  const xmlTemplateRows = <Query query={QUERY_XML_TEMPLATES}>
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Updated At',
+      dataIndex: 'updatedAt',
+      key: 'updatedId'
+    },
+    {
+      title: 'Created At',
+      dataIndex: 'createdAt',
+      key: 'createdAt'
+    },
+    {
+      title: 'Used in',
+      dataIndex: 'usedIn',
+      key: 'usedIn'
+    },
+    {
+      title: 'Actions',
+      dataIndex: 'actions',
+      key: 'actions'
+    }
+  ];
+
+  const table:any = <Query query={QUERY_XML_TEMPLATES}>
     {
       ({ data }) => {
         const xmlTemplates = data.xmlTemplates ? data.xmlTemplates : [];
+        const dataSource = xmlTemplates.map((xmlTemplate:any) => (
+          {
+            key: xmlTemplate.id,
+            name: <Link to={`/xml_templates/${xmlTemplate.id}`}>{xmlTemplate.name}</Link>,
+            updatedAt: xmlTemplate.updatedAt,
+            createdAt: xmlTemplate.createdAt,
+            usedIn: xmlTemplate.templatesCount,
+            actions: <Link to={`/xml_templates/${xmlTemplate.id}/edit`}>Edit</Link>
+          }
+        ));
 
-        return(xmlTemplates.map((xmlTemplate:any) => (
-          <tr key={xmlTemplate.id}>
-            <td>{xmlTemplate.name}</td>
-            <td>{xmlTemplate.createdAt}</td>
-            <td>{xmlTemplate.templatesCount} templates</td>
-            <td>
-              <Link to={`/xml_templates/${xmlTemplate.id}`}>Show</Link>
-            </td>
-            <td>
-            <Link to={`/xml_templates/${xmlTemplate.id}/edit`}>Edit</Link>
-            </td>
-          </tr>
-        )));
+        return(
+          <div>
+            <Table dataSource={dataSource} columns={columns} pagination={{ pageSize: 25 }} />
+          </div>
+        );
       }
     }
   </Query>
@@ -30,21 +61,7 @@ const XmlTemplateIndex = () => {
   return(
     <div>
       <Link to='/xml_templates/new'>New XML Template</Link>
-
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Created At</th>
-            <th>Used in</th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {xmlTemplateRows}
-        </tbody>
-      </table>
+      {table}
     </div>
   );
 };
