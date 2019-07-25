d4087328929229a461a45d6ad6f6e9401e4aa0d3
Michal Macejko <michal.macejko@3scale.net>
2019-07-25 10:32:02 +0200

    [XML templates] delete action

--- app/modules/XmlTemplateIndex/XmlTemplateIndex.tsx
+++ app/modules/XmlTemplateIndex/XmlTemplateIndex.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import { QUERY_XML_TEMPLATES } from 'store/app';
+import { MUTATION_DELETE_XML_TEMPLATE } from './store';
 import { Link } from 'react-router-dom';
-import { Query } from 'react-apollo';
+import { Query, Mutation } from 'react-apollo';
 import { Table, Button } from 'antd';
 import moment from 'moment';
 import s from './XmlTemplateIndex.scss';
@@ -40,9 +41,14 @@ const XmlTemplateIndex = () => {
       key: 'usedIn'
     },
     {
-      title: 'Actions',
-      dataIndex: 'actions',
-      key: 'actions'
+      title: 'Edit',
+      dataIndex: 'edit',
+      key: 'edit'
+    },
+    {
+      title: 'Delete',
+      dataIndex: 'delete',
+      key: 'delete'
     }
   ];
 
@@ -59,7 +65,27 @@ const XmlTemplateIndex = () => {
             updatedAt: moment(xmlTemplate.updatedAt).format('LLL'),
             createdAt: moment(xmlTemplate.createdAt).format('LLL'),
             usedIn: xmlTemplate.templatesCount,
-            actions: <Link to={`/xml_templates/${xmlTemplate.id}/edit`}>Edit</Link>
+            edit: <Link to={`/xml_templates/${xmlTemplate.id}/edit`}>Edit</Link>,
+            delete: (
+              <Mutation mutation={MUTATION_DELETE_XML_TEMPLATE} update={(store) => {
+                const xmlTemplatesQuery:any = store.readQuery({ query: QUERY_XML_TEMPLATES });
+                const xmlTemplates = data.xmlTemplates ? data.xmlTemplates : [];
+                const editedXmlTemplates = xmlTemplates.filter((obj:any) => obj.id !== xmlTemplate.id);
+                store.writeQuery({ query: QUERY_XML_TEMPLATES, data: { xmlTemplates: editedXmlTemplates }});
+              }}>
+                {
+                  deleteXmlTemplate => (
+                    <Link to='#' onClick={() => {
+                      if(confirm('Delete?')) {
+                        deleteXmlTemplate({ variables: { id: xmlTemplate.id }})
+                      }
+                    }}>
+                      Delete
+                    </Link>
+                  )
+                }
+              </Mutation>
+            )
           }
         ));
 
